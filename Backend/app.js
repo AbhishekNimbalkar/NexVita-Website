84d1ff9bbfe82cc6b2acb80b778fbe07ff0a93f5
Patel Neel <pneel2710@gmail.com>
2025-07-11 17:56:20 +0530

    NexVita-v2

--- Backend/app.js
+++ Backend/app.js
@@ -3,20 +3,29 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const path = require("path");
-const adminRoutes = require("./routes/adminRoutes"); // New admin routes
+
+const adminRoutes = require("./routes/adminRoutes");
 const companyLogoRoutes = require("../Backend/routes/companyLogoRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 const MONGO_URI = process.env.MONGO_URI;
 
+// CORS Configuration
+const corsOptions = {
+  origin: "http://localhost:5173", // Only allow your frontend origin
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true, // Allow cookies to be sent
+  optionsSuccessStatus: 204, // For legacy browser support
+};
+
 // Middleware
-app.use(cors());
-app.use(express.json()); // For parsing application/json
+app.use(cors(corsOptions)); // Use the CORS configuration
+app.use(express.json());
 app.use(
   "/uploads",
   express.static(path.join(__dirname, process.env.UPLOAD_FOLDER))
-); // Serve static files
+);
 
 // MongoDB Connection
 mongoose
@@ -26,13 +35,13 @@ mongoose
 
 // Routes
 app.use("/api/company-logos", companyLogoRoutes);
-app.use("/api/admin", adminRoutes); // Use the new admin routes
+app.use("/api/admin", adminRoutes);
 
 app.get("/", (req, res) => {
   res.send("Work Process API is running");
 });
 
-// Error handling middleware (optional, but good practice)
+// Error handling middleware
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).send("Something broke!");
